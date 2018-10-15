d5635848c7775b2be87628ad3d94c2cc73f7c4fa
shahzadahmed1195 <44030615+shahzadahmed1195@users.noreply.github.com>
2018-10-15 19:39:39 -0400

    Y
    changed BACK to webdriver : driver

--- src/test/java/CucumberTesty/ShahzadAhmedCucumber/StepDefinition.java
+++ src/test/java/CucumberTesty/ShahzadAhmedCucumber/StepDefinition.java
@@ -8,7 +8,7 @@ import junit.framework.Assert;
 
 public class StepDefinition {
 	
-	WebDriver=ChromeDriver;
+	WebDriver driver;
 
 	@Given("I go to \"([^\"]*)\"$")
 	public void i_go_to(String arg1) {
